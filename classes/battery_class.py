1b8e19a1456f63c8095924c38d640eb5b2d70f88
juliapestel <juliapestel99@gmail.com>
2024-01-11 13:07:05 +0100

    map met classes

---
+++ classes/battery_class.py
@@ -0,0 +1,9 @@
+"""
+Battery class
+"""
+
+class Battery():
+    def __init__(self, pos_x, pos_y, capacity):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.capacity = capacity
---
+++ classes/house_class.py
@@ -0,0 +1,9 @@
+"""
+House class
+"""
+
+class House():
+    def __init__(self, pos_x, pos_y, capacity):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.capacity = capacity
