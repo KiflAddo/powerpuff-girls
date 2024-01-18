b1c76c66a6fea773bb7a12bb20977fbaf4e91340
KiflAddo <kiflatje1@gmail.com>
2024-01-18 14:38:55 +0100

    Experiment functie in main af

--- main.py
+++ main.py
@@ -8,25 +8,47 @@ from classes.grid import Grid
 from classes.greedy_random import Greedy_Random
 from representatie import access_data
 import sys
-import csv
+import pandas as pd
+
+def experiments(number_of_experiments):
+    results = []
+    for experiment in range(number_of_experiments):
+        grid = Grid(houses, batteries)
+        algorithm = Greedy_Random(10, 10, 10, 10, grid)
+        algorithm.smallest_distance()
+        algorithm.step()
+        grid.calculate_costs()
+        results.append(grid.costs)
+
+    df = pd.DataFrame(results)
+    df.index += 1
+    df.columns = ['Cost']
+    df.to_csv('Results1.csv')
+
+def experiments2(number_of_experiments, algorithm):
+    results = []
+    for experiment in range(number_of_experiments):
+        grid = Grid(houses, batteries)
+        algorithm.smallest_distance()
+        algorithm.step()
+        grid.calculate_costs()
+        results.append(grid.costs)
+
+    df = pd.DataFrame(results)
+    df.index += 1
+    df.columns = ['Cost']
+    df.to_csv('Results2.csv')
 
 if __name__ == "__main__":
     batteries = access_data('huizen_batterijen/district_2/district-2_batteries.csv')
 
     houses = access_data('huizen_batterijen/district_2/district-2_houses.csv')
-    number_experiments = 10
-    results = []
-    for experiment in range(1, number_experiments + 1):
-        single_experiment = {}
-        grid = Grid(houses, batteries)
-        algo = Greedy_Random(10, 10, 10, 10, grid)
-        algo.smallest_distance()
-        algo.step()
-        grid.calculate_costs()
-        single_experiment[experiment] = grid.costs
-        results.append(single_experiment)
+    # grid = Grid(houses, batteries)
+    algorithm = Greedy_Random(10, 10, 10, 10, grid)
 
-    print(results)
+    # experiments(10)
+    experiments2(10, algorithm)
+
 
     # grid1.setup_plot()
     # grid1.visualize()
