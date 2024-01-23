c8dd3b323e92257b2a381562a218c82ab0b560fb
maxbroos <maxbernhardroos@gmail.com>
2024-01-23 12:25:51 +0100

    new visualizing histogram

--- visualize_cost.py
+++ visualize_cost.py
@@ -3,26 +3,16 @@ import seaborn as sns
 import pandas as pd
 from pprint import pprint
 
-def visualize_cost(filename):
-    '''
-    1. dataframe komt binnen (kosten, battrijpositie)
-    2. dataframe ascending order
-    3. visualiseer
+def visualize_cost(input_filename, output_filename):
+    ''' Visualize the cost with a histogram that counts how many times they appear'''
 
-    '''
+    df = pd.read_csv(input_filename, sep=',')
 
-    df = pd.read_csv(filename, sep=',')
+    plt.hist(df['Costs'], bins=50)
+    plt.xlabel("Costs")
+    plt.ylabel("Count")
+    plt.title("Experiment Costs")
 
-    # sort with cost ascending
-    sorted_df = df.sort_values(by='Costs')
-    pprint(sorted_df)
-
-    # # get the minimumcost
-    # min_index = df.idxmin()
-
-    # visualise the cost
-    sns.catplot(data=sorted_df, x='Experiment', y='Costs', kind='bar')
-    plt.title('Experiment Costs', loc='center')
-    # plt.bar(min_index, df[min_index], color='red')
-    # plt.legend('hai')
+    # Save the plot to a file
+    plt.savefig(output_filename)
     plt.show()
